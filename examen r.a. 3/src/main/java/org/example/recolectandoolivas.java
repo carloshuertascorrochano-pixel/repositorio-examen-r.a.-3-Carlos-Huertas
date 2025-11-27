adf71bc91dd20c8fffe004b98f6fe7b2d0c21d5a
A711\dam1 <carloshuertascorrochano@gmail.com>
2025-11-27 19:22:57 +0100

    caca

--- examen r.a. 3/src/main/java/org/example/recolectandoolivas.java
+++ examen r.a. 3/src/main/java/org/example/recolectandoolivas.java
@@ -68,36 +68,6 @@ public class recolectandoolivas {
         System.out.println();
 
 
-//        int bloq_horizontal = (matriz[0].length/8);
-//        int bloq_vertical = (matriz.length/8);
-//
-//        int aux_bloq = 0;
-//
-//        int aux_8 = 8;
-//        int aux_ini = 0;
-//        int aux_ini_vert = 0;
-//        int aux_8_vert = 8;
-//
-//
-//        do {
-//            for (int i = aux_ini; i < aux_8; i++) {
-//                for (int j = aux_ini_vert; j < aux_8_vert; j++) {
-//                    System.out.print(matriz[i][j] + " ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
-//            aux_8 += 8;
-//            aux_ini += 8;
-//            aux_bloq++;
-//            if (aux_bloq == bloq_vertical) {
-//                aux_bloq = 0;
-//                aux_ini_vert += 8;
-//                aux_8_vert += 8;
-//                aux_8 = 8;
-//                aux_ini = 0;
-//            }
-//        } while (aux_8 <= matriz.length && aux_8_vert <= matriz[0].length);
 
     }
 }
